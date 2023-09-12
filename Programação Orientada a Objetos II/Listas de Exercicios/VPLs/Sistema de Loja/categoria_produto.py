34451775caf2a4d6809ff2d8079fdfb77986753a
Pedro Gimenez <pehqge@gmail.com>
2023-09-12 14:56:50 -0300

    Adicionado exercicio VPL 1

--- Programação Orientada a Objetos II/Listas de Exercicios/VPLs/Sistema de Loja/categoria_produto.py
+++ Programação Orientada a Objetos II/Listas de Exercicios/VPLs/Sistema de Loja/categoria_produto.py
@@ -1,8 +1,11 @@
-
-
 class CategoriaProduto:
-
     def __init__(self, titulo):
         self.__titulo = titulo
 
-    """Insira aqui os demais metodos ... """
+    @property
+    def titulo(self):
+        return self.__titulo
+    
+    @titulo.setter
+    def titulo(self, titulo):
+        self.__titulo = titulo
--- Programação Orientada a Objetos II/Listas de Exercicios/VPLs/Sistema de Loja/cliente.py
+++ Programação Orientada a Objetos II/Listas de Exercicios/VPLs/Sistema de Loja/cliente.py
@@ -1,11 +1,20 @@
-
-
-class ...
-
-    """Insira aqui o construtor"""
+class Cliente:
+    def __init__(self, nome, fone):
+        self.__nome = nome
+        self.__fone = fone
 
     @property
     def nome(self):
         return self.__nome
 
-    """Insira aqui os demais metodos ... """
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def fone(self):
+        return self.__fone
+
+    @fone.setter
+    def fone(self, fone):
+        self.__fone = fone
